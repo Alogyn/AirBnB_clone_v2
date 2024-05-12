be8d6e362915d476c5b78ca5082d630ca9d46f7a
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-05-12 16:27:28 -0700

    0x02. AirBnB clone - MySQL

--- models/place.py
+++ models/place.py
@@ -82,5 +82,5 @@ class Place(BaseModel, Base):
 
         @amenities.setter
         def amenities(self, value):
-            if isinstance(value, Amenity):
+            if type(value) == Amenity:
                 self.amenity_ids.append(value.id)
