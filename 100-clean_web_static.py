9ff813d4c1b9f2e14fb3290c7dddb772beb7a8d8
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-05-16 15:33:24 -0700

    Deletes out-of-date archives, using the function do_clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """
 Deletes out-of-date archives
-fab -f 100-clean_web_static.py do_clean:number=2
-    -i ssh-key -u ubuntu > /dev/null 2>&1
+fab -f 100-clean_web_static.py do_clean:number=2 -i ssh-key -u ubuntu > /dev/null 2>&1
 """
 
 import os
 from fabric.api import *
 
 env.hosts = ['100.27.2.72', '54.158.79.158']
-
 
 def do_clean(number=0):
     """Delete out-of-date archives.
@@ -21,13 +19,20 @@ def do_clean(number=0):
     """
     number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    # Local cleanup
+    local_archives = sorted(os.listdir("versions"))
+    archives_to_delete = local_archives[:-number]
+    
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
+        for archive in archives_to_delete:
+            local("rm ./{}".format(archive))
+    
+    # Remote cleanup
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        remote_archives = run("ls -tr").split()
+        remote_archives = [a for a in remote_archives if "web_static_" in a]
+        archives_to_delete = remote_archives[:-number]
+        
+        for archive in archives_to_delete:
+            run("rm -rf ./{}".format(archive))
+
